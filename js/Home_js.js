13bff48222dd3067526a42913e9c80d2fd4f46c3
Clayton Feathers <Clayton Feathers>
2017-01-31 22:54:53 -0500

    second change

--- js/Home_js.js
+++ js/Home_js.js
@@ -1,3 +1,15 @@
 function myFunction(){
 	$(".dropdown-content").fadeToggle();
 }
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("menu_nav").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("menu_nav").style.marginLeft = "0";
+}
